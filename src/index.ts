8b378be586d4f541442a6215c7a85031e22958c3
Chiciuc Nicușor <nicuchiciuc@gmail.com>
2019-10-08 01:23:10 +0300

    👌 Added function for circumcircle and bisectors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 console.log("fuck");
 
-type Point = { x: number; y: number; id: string };
-type Line = { p1: Point; p2: Point; id: string };
+type Point = { x: number; y: number; id?: string };
+type Line = { p1: Point; p2: Point; id?: string };
+type LineFormula = { slope: number; n: number };
+type Circle = { center: Point; rad: number; id?: string };
 
-type Triangle = { p1: Point; p2: Point; p3: Point; id: string };
+type Triangle = { p1: Point; p2: Point; p3: Point; id?: string };
 
 window.onload = event => {
   const canvas = document.getElementById("canvas");
   if (!canvas || !(canvas instanceof HTMLCanvasElement)) return;
   const ctx = canvas.getContext("2d");
+
+  window.ctx = ctx;
 
   canvas.onclick = event => {
     const p: Point = {
@@ -27,15 +31,15 @@ window.onload = event => {
   });
 
   const superTriangle: Triangle = {
-    p1: { x: 400, y: 5, id: guid() },
-    p2: { x: 5, y: 595, id: guid() },
-    p3: { x: 790, y: 595, id: guid() },
+    p1: { x: 400, y: 100, id: guid() },
+    p2: { x: 100, y: 400, id: guid() },
+    p3: { x: 700, y: 500, id: guid() },
     id: guid()
   };
 
   const triangles: Triangle[] = [superTriangle];
 
-  drawTri(ctx, superTriangle);
+  redrawAll();
 
   // points.forEach(addPointToDelaunay);
 
@@ -69,6 +73,11 @@ window.onload = event => {
     });
 
     triangles.forEach(tri => {
+      const c = triangleCircumcircle(tri);
+
+      console.log(c);
+
+      drawCircle(ctx, c);
       drawTri(ctx, tri);
     });
   }
@@ -92,6 +101,7 @@ const pointInsideTriangle = ({ p1, p2, p3 }: Triangle, p: Point) => {
 const drawPoint = (ctx: CanvasRenderingContext2D, { x, y }: Point) => {
   ctx.beginPath();
   ctx.arc(x, y, 5, 0, Math.PI * 2);
+  ctx.closePath();
   ctx.fill();
 };
 
@@ -99,6 +109,8 @@ const drawLine = (ctx: CanvasRenderingContext2D, { p1, p2 }: Line) => {
   ctx.beginPath();
   ctx.moveTo(p1.x, p1.y);
   ctx.lineTo(p2.x, p2.y);
+  ctx.closePath();
+
   ctx.stroke();
 };
 
@@ -106,6 +118,61 @@ const drawTri = (ctx: CanvasRenderingContext2D, { p1, p2, p3 }: Triangle) => {
   drawLine(ctx, { p1: p1, p2: p2, id: guid() });
   drawLine(ctx, { p1: p2, p2: p3, id: guid() });
   drawLine(ctx, { p1: p3, p2: p1, id: guid() });
+};
+
+const drawCircle = (
+  ctx: CanvasRenderingContext2D,
+  { center: { x, y }, rad }: Circle
+) => {
+  ctx.beginPath();
+  ctx.arc(x, y, rad, 0, Math.PI * 2);
+  ctx.closePath();
+
+  ctx.stroke();
+};
+
+const lineFormulaFromPoints = ({ p1, p2 }: Line): LineFormula => {
+  const slope = (p2.y - p1.y) / (p2.x - p1.x);
+  const n = p1.y - slope * p1.x;
+
+  return { slope, n };
+};
+
+const segmentBisector = (l: Line) => {
+  const { slope, n } = lineFormulaFromPoints(l);
+
+  const { p1, p2 } = l;
+  const middle: Point = {
+    x: (p2.x + p1.x) / 2,
+    y: (p2.y + p1.y) / 2,
+    id: guid()
+  };
+
+  const d = (slope + 1 / slope) * middle.x + n;
+
+  return { slope: -(1 / slope), n: d };
+};
+
+const triangleCircumcircle = (tri: Triangle): Circle => {
+  const bis1 = segmentBisector({ p1: tri.p1, p2: tri.p2, id: guid() });
+  const bis2 = segmentBisector({ p1: tri.p2, p2: tri.p3, id: guid() });
+
+  const x = (bis2.n - bis1.n) / (bis1.slope - bis2.slope);
+  const y = bis1.slope * x + bis1.n;
+
+  const center: Point = { x, y, id: guid() };
+
+  const rad = pointDist(tri.p1, center);
+
+  return { rad, center, id: guid() };
+};
+
+const pointDist = (p1: Point, p2: Point): number => {
+  return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
+};
+
+const yFunc = (form: LineFormula, x: number): number => {
+  return x * form.slope + form.n;
 };
 
 function guid() {
